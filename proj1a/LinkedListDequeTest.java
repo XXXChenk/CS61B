f4af68f4429154f1a0f89e8f5d4f88ca8cdb4e83
XXXChenk <xxxchenk@gmail.com>
2026-04-08 11:24:19 +0800

    修改了code style

--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -51,7 +51,8 @@ public class LinkedListDequeTest {
      */
     public static void addIsEmptySizeTest() {
         System.out.println("Running add/isEmpty/Size test.");
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+        System.out.println("Make sure to uncomment the lines below "
+                + "(and delete this print statement).");
         /*
         LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
 
@@ -82,7 +83,8 @@ public class LinkedListDequeTest {
 
         System.out.println("Running add/remove test.");
 
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+        System.out.println("Make sure to uncomment the lines below "
+                + "(and delete this print statement).");
         /*
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
         // should be empty
